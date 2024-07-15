52795a21902d5ca54a45f038714ddec32aa59ede
Omar Martinez <fmartinez@devbot.com>
2024-07-15 12:51:45 -0600

    "Presence" For Steam Lobbies

--- Source/PuzzlePlatform/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatform/PuzzlePlatformsGameInstance.cpp
@@ -79,9 +79,11 @@ void UPuzzlePlatformsGameInstance::CreateSession()
 {
     if (SessionInterface.IsValid()) {
         FOnlineSessionSettings SessionSettings;
-        SessionSettings.bIsLANMatch = true;
+        SessionSettings.bIsLANMatch = false;
         SessionSettings.NumPublicConnections = 2;
         SessionSettings.bShouldAdvertise = true;
+        SessionSettings.bUsesPresence = true;
+        SessionSettings.bUseLobbiesIfAvailable = true;
 
         SessionInterface->CreateSession(0, SESSION_NAME, SessionSettings);
     }
@@ -117,6 +119,8 @@ void UPuzzlePlatformsGameInstance::RefreshServerList()
     if (SessionSearch.IsValid())
     {
         //SessionSearch->bIsLanQuery = true;
+        SessionSearch->MaxSearchResults = 100;
+        SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
         UE_LOG(LogTemp, Warning, TEXT("Starting Find Session"));
         SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
     }
